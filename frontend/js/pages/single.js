c9c87bc1cbda7d39212a030d2a4f3f7949589613
Nathan Akpawu <nakpawu@gmail.com>
2021-04-01 21:18:03 +0000

    Single product and page 404

---
+++ frontend/js/pages/single.js
@@ -0,0 +1,30 @@
+loadConfig().then(data => {
+    config = data;
+    const productId = getProductId();
+    fetch(config.host + 'api/teddies/' + productId)
+    .then(data => data.json())
+    .then(jsonProduct => {
+        let product = new Product(jsonProduct);
+        document.querySelector('.single-product').innerHTML = `
+        <div class="col-lg-6 col-xs-12">
+            <img src="${product.imageUrl}" class="card-img-top" alt="${product.name}">
+        </div>
+        <div class="col-lg-6 col-xs-12">
+            <h1 class="single-title">${product.name}</h1>
+            <h2 class="single-price">Prix : <strong>${product.formatPrice()} &euro;</strong></h2>
+            <p class="single-description">${product.description}</p>
+            <select class="single-colors form-select">
+                <option selected>Choisir la couleur</option>
+            </select>
+            <div class="buttons">
+                <a href="cart.html" class="btn btn-purple btn-lg rounded-pill"><i class="bi bi-plus-circle"></i> Ajouter au panier</a>
+                <a href="index.html" class="btn btn-purple btn-lg rounded-pill"><i class="bi bi-eye"></i> Autres produits</a>
+                </div>
+        </div>`
+        product.getColors();
+    })
+    .catch(error => {
+        console.log(error);
+        redirect404();
+    });
+});
--- frontend/js/product.js
+++ frontend/js/product.js
@@ -6,4 +6,14 @@ class Product {
     formatPrice() {
         return (this.price / 100).toFixed(2);
     }
+
+    getColors() {
+        let colors = this.colors;
+        if (colors.length > 0) {
+            for (const [i, color] of colors.entries()) {
+                document.querySelector('.single-colors').innerHTML += `
+                <option value="${i + 1}">${color}</option>`
+            }
+        }
+    }
 }
